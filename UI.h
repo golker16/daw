b88b1708d0b7e56d4894bbf3e46f9b562edc2cbd
golker16 <gega1609@gmail.com>
2026-04-21 04:12:37 -0500

    Update UI.h

--- UI.h
+++ UI.h
@@ -114,6 +114,93 @@ public:
         UiDocumentState document{};
     };
 
+    struct VisiblePluginDescriptor
+    {
+        std::string name;
+        std::string vendor;
+        std::string format;
+        std::string processModeLabel;
+        std::uint32_t latencySamples = 0;
+        bool supportsDoublePrecision = false;
+        bool supportsSampleAccurateAutomation = false;
+    };
+
+    struct PluginManagerState
+    {
+        bool visible = false;
+        std::string searchText;
+        std::size_t selectedIndex = 0;
+        std::vector<VisiblePluginDescriptor> loadedPlugins;
+        std::vector<VisiblePluginDescriptor> filteredPlugins;
+        std::vector<std::string> searchPaths;
+        std::vector<std::string> favorites;
+        std::vector<std::string> blacklist;
+        std::string statusText;
+    };
+
+    enum class SurfaceKind
+    {
+        None,
+        Browser,
+        ChannelRack,
+        StepSequencer,
+        PianoRoll,
+        Playlist,
+        Mixer,
+        Plugin
+    };
+
+    struct SurfaceRect
+    {
+        int x = 0;
+        int y = 0;
+        int width = 0;
+        int height = 0;
+    };
+
+    struct UiClipVisual
+    {
+        std::uint32_t clipId = 0;
+        std::uint32_t trackId = 0;
+        SurfaceRect rect{};
+        bool selected = false;
+    };
+
+    struct UiNoteVisual
+    {
+        int lane = 0;
+        int step = 0;
+        SurfaceRect rect{};
+        bool selected = false;
+    };
+
+    struct SurfaceInteractionState
+    {
+        SurfaceKind activeSurface = SurfaceKind::None;
+        bool mouseDown = false;
+        bool draggingClip = false;
+        bool draggingNote = false;
+        bool marqueeActive = false;
+        bool browserDragActive = false;
+        POINT dragStart{};
+        POINT dragCurrent{};
+        std::uint32_t selectedClipId = 0;
+        std::size_t selectedNoteIndex = static_cast<std::size_t>(-1);
+        std::size_t selectedBrowserItemIndex = static_cast<std::size_t>(-1);
+        RECT marqueeRect{};
+        std::vector<UiClipVisual> playlistClipVisuals;
+        std::vector<UiNoteVisual> pianoNoteVisuals;
+    };
+
+    struct DockedPaneState
+    {
+        WorkspacePane pane = WorkspacePane::Playlist;
+        RECT bounds{};
+        bool detached = false;
+        bool visible = true;
+        std::string title;
+    };
+
     enum class WorkspacePane
     {
         Browser,
@@ -122,6 +209,16 @@ public:
         Playlist,
         Mixer,
         Plugin
+    };
+
+    enum class EditorTool
+    {
+        Draw,
+        Paint,
+        Select,
+        DeleteTool,
+        Slice,
+        Mute
     };
 
     struct WorkspaceState
@@ -137,6 +234,12 @@ public:
         int activePattern = 1;
         double tempoBpm = 120.0;
         std::size_t snapIndex = 2;
+        std::size_t browserTabIndex = 0;
+        std::size_t playlistZoomIndex = 2;
+        std::size_t pianoZoomIndex = 2;
+        EditorTool playlistTool = EditorTool::Draw;
+        EditorTool pianoTool = EditorTool::Draw;
+        WorkspacePane focusedPane = WorkspacePane::Playlist;
     };
 
 public:
@@ -145,12 +248,24 @@ public:
 
 private:
     static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    static LRESULT CALLBACK PluginManagerProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    static LRESULT CALLBACK SurfaceProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
     void registerWindowClass();
     void createMainWindow();
     void createMainMenu();
     void createControls();
     void layoutControls();
+    void ensurePluginManagerWindow();
+    void layoutPluginManagerWindow();
+    void refreshPluginManagerState();
+    void updatePluginManagerControls();
+    void showPluginManagerWindow();
+    void closePluginManagerWindow();
+    void invalidateSurface(HWND surface);
+    void invalidateAllSurfaces();
+    void ensureDetachedWindows();
+    void updateDockingModel();
 
     void startUiTimer();
     void stopUiTimer();
@@ -185,8 +300,11 @@ private:
     void requestRedo();
     void requestSaveProject();
     void requestLoadProject();
+    void requestCreatePluginStub();
+    void requestTogglePluginSandboxMode();
 
     void handleCommand(WORD commandId);
+    void handlePluginManagerCommand(WORD commandId);
     bool handleKeyDown(WPARAM wParam, LPARAM lParam);
     void showAboutDialog() const;
     void selectNextTrack();
@@ -194,12 +312,53 @@ private:
     void selectNextPattern();
     void selectPreviousPattern();
     void cycleSnap(int delta);
+    void cycleZoom(bool pianoRoll, int delta);
+    void cycleBrowserTab(int delta);
+    void cycleEditorTool(bool pianoRoll, int delta);
     void togglePane(WorkspacePane pane);
     std::string currentSnapLabel() const;
+    std::string currentBrowserTabLabel() const;
+    std::string currentZoomLabel(bool pianoRoll) const;
+    std::string currentToolLabel(bool pianoRoll) const;
     void setStaticText(HWND control, const std::string& text) const;
+    std::string buildBrowserPanelText() const;
+    std::string buildChannelRackPanelText() const;
+    std::string buildStepSequencerPanelText() const;
+    std::string buildPianoRollPanelText() const;
+    std::string buildPlaylistPanelText() const;
+    std::string buildMixerPanelText() const;
+    std::string buildPluginPanelText() const;
+    std::string buildPluginManagerDetailText() const;
+    std::string buildPluginManagerPathText() const;
+    void applyPluginSearchFilter();
+    std::string buildPluginManagerTableText() const;
+    SurfaceKind kindFromSurfaceHandle(HWND hwnd) const;
+    void paintSurface(HWND hwnd, SurfaceKind kind);
+    void paintBrowserSurface(HDC dc, const RECT& rect);
+    void paintChannelRackSurface(HDC dc, const RECT& rect);
+    void paintStepSequencerSurface(HDC dc, const RECT& rect);
+    void paintPianoRollSurface(HDC dc, const RECT& rect);
+    void paintPlaylistSurface(HDC dc, const RECT& rect);
+    void paintMixerSurface(HDC dc, const RECT& rect);
+    void paintPluginSurface(HDC dc, const RECT& rect);
+    void handleSurfaceMouseDown(HWND hwnd, SurfaceKind kind, int x, int y);
+    void handleSurfaceMouseMove(HWND hwnd, SurfaceKind kind, int x, int y, WPARAM flags);
+    void handleSurfaceMouseUp(HWND hwnd, SurfaceKind kind, int x, int y);
+    void rebuildPlaylistVisuals(const RECT& rect);
+    void rebuildPianoVisuals(const RECT& rect);
+    int clampValue(int value, int minValue, int maxValue) const;
+    bool isPaneVisible(WorkspacePane pane) const;
+    void setPaneVisible(WorkspacePane pane, bool visible);
+    DockedPaneState* findDockedPane(WorkspacePane pane);
+    const DockedPaneState* findDockedPane(WorkspacePane pane) const;
+    void detachPane(WorkspacePane pane);
+    void attachPane(WorkspacePane pane);
+    std::string paneTitle(WorkspacePane pane) const;
 
 private:
     static constexpr const char* kWindowClassName = "DAWCloudTemplateWindowClass";
+    static constexpr const char* kPluginManagerClassName = "DAWCloudPluginManagerClass";
+    static constexpr const char* kSurfaceClassName = "DAWCloudSurfaceClass";
     static constexpr const char* kWindowTitleBase = "DAW Cloud Template";
     static constexpr UINT_PTR kUiTimerId = 1;
     static constexpr UINT kUiTimerIntervalMs = 250;
@@ -241,6 +400,21 @@ private:
         IdButtonMenuPlaylist = 1033,
         IdButtonMenuMixer = 1034,
         IdButtonMenuPlugin = 1035,
+        IdButtonBrowserTabPrev = 1036,
+        IdButtonBrowserTabNext = 1037,
+        IdButtonPlaylistZoomPrev = 1038,
+        IdButtonPlaylistZoomNext = 1039,
+        IdButtonPianoZoomPrev = 1040,
+        IdButtonPianoZoomNext = 1041,
+        IdButtonPlaylistToolPrev = 1042,
+        IdButtonPlaylistToolNext = 1043,
+        IdButtonPianoToolPrev = 1044,
+        IdButtonPianoToolNext = 1045,
+        IdButtonManagePlugins = 1046,
+        IdButtonPluginRescan = 1047,
+        IdButtonPluginAddStub = 1048,
+        IdButtonPluginToggleSandbox = 1049,
+        IdButtonPluginClose = 1050,
 
         IdCheckboxAutomation = 1101,
         IdCheckboxPdc = 1102,
@@ -261,7 +435,17 @@ private:
         IdLabelPlugin = 1213,
         IdLabelProjectSummary = 1214,
         IdLabelDocument = 1215,
-        IdLabelSelection = 1216
+        IdLabelSelection = 1216,
+        IdLabelBrowserHeader = 1217,
+        IdLabelChannelHeader = 1218,
+        IdLabelPianoHeader = 1219,
+        IdLabelPlaylistHeader = 1220,
+        IdLabelMixerHeader = 1221,
+        IdLabelPluginHeader = 1222,
+        IdLabelContext = 1223,
+        IdLabelPluginManagerHeader = 1224,
+        IdLabelPluginManagerDetail = 1225,
+        IdLabelPluginManagerPaths = 1226
     };
 
     enum MenuId : WORD
@@ -287,11 +471,12 @@ private:
         IdMenuOptionsPdc = 2019,
         IdMenuOptionsAnticipative = 2020,
         IdMenuOptionsPatSong = 2021,
-        IdMenuToolsStartEngine = 2022,
-        IdMenuToolsStopEngine = 2023,
-        IdMenuToolsRebuildGraph = 2024,
-        IdMenuToolsRenderOffline = 2025,
-        IdMenuHelpAbout = 2026
+        IdMenuOptionsManagePlugins = 2022,
+        IdMenuToolsStartEngine = 2023,
+        IdMenuToolsStopEngine = 2024,
+        IdMenuToolsRebuildGraph = 2025,
+        IdMenuToolsRenderOffline = 2026,
+        IdMenuHelpAbout = 2027
     };
 
 private:
@@ -331,6 +516,7 @@ private:
     HWND nextTrackButton_ = nullptr;
     HWND rebuildGraphButton_ = nullptr;
     HWND renderOfflineButton_ = nullptr;
+    HWND managePluginsButton_ = nullptr;
 
     HWND automationCheckbox_ = nullptr;
     HWND pdcCheckbox_ = nullptr;
@@ -347,22 +533,54 @@ private:
     HWND selectionLabel_ = nullptr;
 
     HWND browserMenuButton_ = nullptr;
+    HWND browserTabPrevButton_ = nullptr;
+    HWND browserTabNextButton_ = nullptr;
+    HWND browserHeaderLabel_ = nullptr;
     HWND browserPanel_ = nullptr;
     HWND channelRackMenuButton_ = nullptr;
+    HWND channelHeaderLabel_ = nullptr;
     HWND channelRackPanel_ = nullptr;
     HWND stepSequencerPanel_ = nullptr;
     HWND pianoRollMenuButton_ = nullptr;
+    HWND pianoZoomPrevButton_ = nullptr;
+    HWND pianoZoomNextButton_ = nullptr;
+    HWND pianoToolPrevButton_ = nullptr;
+    HWND pianoToolNextButton_ = nullptr;
+    HWND pianoHeaderLabel_ = nullptr;
     HWND pianoRollPanel_ = nullptr;
     HWND playlistMenuButton_ = nullptr;
+    HWND playlistZoomPrevButton_ = nullptr;
+    HWND playlistZoomNextButton_ = nullptr;
+    HWND playlistToolPrevButton_ = nullptr;
+    HWND playlistToolNextButton_ = nullptr;
+    HWND playlistHeaderLabel_ = nullptr;
     HWND playlistPanel_ = nullptr;
     HWND mixerMenuButton_ = nullptr;
+    HWND mixerHeaderLabel_ = nullptr;
     HWND mixerPanel_ = nullptr;
     HWND pluginMenuButton_ = nullptr;
+    HWND pluginHeaderLabel_ = nullptr;
     HWND pluginPanel_ = nullptr;
+    HWND contextLabel_ = nullptr;
+
+    HWND pluginManagerHwnd_ = nullptr;
+    HWND pluginManagerHeaderLabel_ = nullptr;
+    HWND pluginManagerSearchEdit_ = nullptr;
+    HWND pluginManagerListBox_ = nullptr;
+    HWND pluginManagerDetailLabel_ = nullptr;
+    HWND pluginManagerPathsLabel_ = nullptr;
+    HWND pluginManagerRescanButton_ = nullptr;
+    HWND pluginManagerAddStubButton_ = nullptr;
+    HWND pluginManagerToggleSandboxButton_ = nullptr;
+    HWND pluginManagerCloseButton_ = nullptr;
 
     mutable VisibleEngineState visibleState_{};
+    PluginManagerState pluginManagerState_{};
+    SurfaceInteractionState interactionState_{};
+    std::vector<DockedPaneState> dockedPanes_{};
     WorkspaceState workspace_{};
     std::size_t selectedTrackIndex_ = 0;
 };
+
 
 
