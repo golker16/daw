475155b1a9c113a0f257109a276b410b995b4e0d
golker16 <gega1609@gmail.com>
2026-04-21 03:30:57 -0500

    Update UI.h

--- UI.h
+++ UI.h
@@ -114,6 +114,31 @@ public:
         UiDocumentState document{};
     };
 
+    enum class WorkspacePane
+    {
+        Browser,
+        ChannelRack,
+        PianoRoll,
+        Playlist,
+        Mixer,
+        Plugin
+    };
+
+    struct WorkspaceState
+    {
+        bool browserVisible = true;
+        bool channelRackVisible = true;
+        bool pianoRollVisible = true;
+        bool playlistVisible = true;
+        bool mixerVisible = true;
+        bool pluginVisible = true;
+        bool songMode = true;
+        bool recordArmed = false;
+        int activePattern = 1;
+        double tempoBpm = 120.0;
+        std::size_t snapIndex = 2;
+    };
+
 public:
     UI(HINSTANCE hInstance, int nCmdShow, AudioEngine& engine);
     int run();
@@ -125,17 +150,21 @@ private:
     void createMainWindow();
     void createMainMenu();
     void createControls();
+    void layoutControls();
 
     void startUiTimer();
     void stopUiTimer();
     void refreshFromEngineSnapshot();
     VisibleEngineState buildVisibleEngineState() const;
 
+    void updateTransportControls();
     void updateStatusLabel();
     void updateMetricLabels();
     void updateProjectLabels();
     void updateToggleStates();
     void updateWindowTitle();
+    void updateWorkspacePanels();
+    void updateViewButtons();
 
     void requestEngineStart();
     void requestEngineStop();
@@ -147,6 +176,7 @@ private:
     void requestToggleAutomation();
     void requestTogglePdc();
     void requestToggleAnticipativeProcessing();
+    void requestTempoChange(double bpm);
     void requestAddTrack();
     void requestAddBus();
     void requestAddClip();
@@ -156,9 +186,15 @@ private:
     void requestLoadProject();
 
     void handleCommand(WORD commandId);
+    bool handleKeyDown(WPARAM wParam, LPARAM lParam);
     void showAboutDialog() const;
     void selectNextTrack();
     void selectPreviousTrack();
+    void selectNextPattern();
+    void selectPreviousPattern();
+    void cycleSnap(int delta);
+    void togglePane(WorkspacePane pane);
+    std::string currentSnapLabel() const;
     void setStaticText(HWND control, const std::string& text) const;
 
 private:
@@ -169,65 +205,92 @@ private:
 
     enum ControlId : WORD
     {
-        IdButtonStart = 1001,
-        IdButtonStop = 1002,
+        IdButtonEngineStart = 1001,
+        IdButtonEngineStop = 1002,
         IdButtonPlay = 1003,
-        IdButtonPause = 1004,
-        IdButtonRebuildGraph = 1005,
-        IdButtonRenderOffline = 1006,
-        IdButtonAddTrack = 1007,
-        IdButtonAddBus = 1008,
-        IdButtonAddClip = 1009,
-        IdButtonUndo = 1010,
-        IdButtonRedo = 1011,
-        IdButtonSaveProject = 1012,
-        IdButtonLoadProject = 1013,
-        IdButtonPrevTrack = 1014,
-        IdButtonNextTrack = 1015,
+        IdButtonStopTransport = 1004,
+        IdButtonRecord = 1005,
+        IdButtonPatSong = 1006,
+        IdButtonTempoDown = 1007,
+        IdButtonTempoUp = 1008,
+        IdButtonPatternPrev = 1009,
+        IdButtonPatternNext = 1010,
+        IdButtonSnapPrev = 1011,
+        IdButtonSnapNext = 1012,
+        IdButtonBrowser = 1013,
+        IdButtonChannelRack = 1014,
+        IdButtonPianoRoll = 1015,
+        IdButtonPlaylist = 1016,
+        IdButtonMixer = 1017,
+        IdButtonPlugin = 1018,
+        IdButtonAddTrack = 1019,
+        IdButtonAddBus = 1020,
+        IdButtonAddClip = 1021,
+        IdButtonUndo = 1022,
+        IdButtonRedo = 1023,
+        IdButtonSaveProject = 1024,
+        IdButtonLoadProject = 1025,
+        IdButtonPrevTrack = 1026,
+        IdButtonNextTrack = 1027,
+        IdButtonRebuildGraph = 1028,
+        IdButtonRenderOffline = 1029,
+        IdButtonMenuBrowser = 1030,
+        IdButtonMenuChannelRack = 1031,
+        IdButtonMenuPianoRoll = 1032,
+        IdButtonMenuPlaylist = 1033,
+        IdButtonMenuMixer = 1034,
+        IdButtonMenuPlugin = 1035,
 
         IdCheckboxAutomation = 1101,
         IdCheckboxPdc = 1102,
         IdCheckboxAnticipative = 1103,
 
         IdLabelStatus = 1201,
-        IdLabelBackend = 1202,
-        IdLabelSampleRate = 1203,
-        IdLabelBlockSize = 1204,
-        IdLabelCpu = 1205,
-        IdLabelXruns = 1206,
-        IdLabelDeadlineMisses = 1207,
-        IdLabelGraphVersion = 1208,
-        IdLabelLatency = 1209,
-        IdLabelTransport = 1210,
-        IdLabelProjectSummary = 1211,
-        IdLabelDocument = 1212,
-        IdLabelSelection = 1213,
-        IdLabelTrackList = 1214,
-        IdLabelBusList = 1215,
-        IdLabelError = 1216
+        IdLabelTempo = 1202,
+        IdLabelPattern = 1203,
+        IdLabelSnap = 1204,
+        IdLabelSystem = 1205,
+        IdLabelHints = 1206,
+        IdLabelBrowser = 1207,
+        IdLabelChannelRack = 1208,
+        IdLabelStepSequencer = 1209,
+        IdLabelPianoRoll = 1210,
+        IdLabelPlaylist = 1211,
+        IdLabelMixer = 1212,
+        IdLabelPlugin = 1213,
+        IdLabelProjectSummary = 1214,
+        IdLabelDocument = 1215,
+        IdLabelSelection = 1216
     };
 
     enum MenuId : WORD
     {
-        IdMenuFileExit = 2001,
-        IdMenuEngineStart = 2002,
-        IdMenuEngineStop = 2003,
-        IdMenuTransportPlay = 2004,
-        IdMenuTransportPause = 2005,
-        IdMenuTransportStop = 2006,
-        IdMenuGraphRebuild = 2007,
-        IdMenuRenderOffline = 2008,
-        IdMenuToggleAutomation = 2009,
-        IdMenuTogglePdc = 2010,
-        IdMenuToggleAnticipative = 2011,
-        IdMenuHelpAbout = 2012,
-        IdMenuProjectAddTrack = 2013,
-        IdMenuProjectAddBus = 2014,
-        IdMenuProjectAddClip = 2015,
-        IdMenuProjectUndo = 2016,
-        IdMenuProjectRedo = 2017,
-        IdMenuProjectSave = 2018,
-        IdMenuProjectLoad = 2019
+        IdMenuFileNew = 2001,
+        IdMenuFileOpen = 2002,
+        IdMenuFileSave = 2003,
+        IdMenuFileExit = 2004,
+        IdMenuEditUndo = 2005,
+        IdMenuEditRedo = 2006,
+        IdMenuAddTrack = 2007,
+        IdMenuAddBus = 2008,
+        IdMenuAddClip = 2009,
+        IdMenuPatternsPrev = 2010,
+        IdMenuPatternsNext = 2011,
+        IdMenuViewBrowser = 2012,
+        IdMenuViewChannelRack = 2013,
+        IdMenuViewPianoRoll = 2014,
+        IdMenuViewPlaylist = 2015,
+        IdMenuViewMixer = 2016,
+        IdMenuViewPlugin = 2017,
+        IdMenuOptionsAutomation = 2018,
+        IdMenuOptionsPdc = 2019,
+        IdMenuOptionsAnticipative = 2020,
+        IdMenuOptionsPatSong = 2021,
+        IdMenuToolsStartEngine = 2022,
+        IdMenuToolsStopEngine = 2023,
+        IdMenuToolsRebuildGraph = 2024,
+        IdMenuToolsRenderOffline = 2025,
+        IdMenuHelpAbout = 2026
     };
 
 private:
@@ -238,12 +301,24 @@ private:
     HWND hwnd_ = nullptr;
     HMENU mainMenu_ = nullptr;
 
-    HWND startButton_ = nullptr;
-    HWND stopButton_ = nullptr;
+    HWND engineStartButton_ = nullptr;
+    HWND engineStopButton_ = nullptr;
     HWND playButton_ = nullptr;
-    HWND pauseButton_ = nullptr;
-    HWND rebuildGraphButton_ = nullptr;
-    HWND renderOfflineButton_ = nullptr;
+    HWND stopTransportButton_ = nullptr;
+    HWND recordButton_ = nullptr;
+    HWND patSongButton_ = nullptr;
+    HWND tempoDownButton_ = nullptr;
+    HWND tempoUpButton_ = nullptr;
+    HWND patternPrevButton_ = nullptr;
+    HWND patternNextButton_ = nullptr;
+    HWND snapPrevButton_ = nullptr;
+    HWND snapNextButton_ = nullptr;
+    HWND browserButton_ = nullptr;
+    HWND channelRackButton_ = nullptr;
+    HWND pianoRollButton_ = nullptr;
+    HWND playlistButton_ = nullptr;
+    HWND mixerButton_ = nullptr;
+    HWND pluginButton_ = nullptr;
     HWND addTrackButton_ = nullptr;
     HWND addBusButton_ = nullptr;
     HWND addClipButton_ = nullptr;
@@ -253,29 +328,39 @@ private:
     HWND loadProjectButton_ = nullptr;
     HWND prevTrackButton_ = nullptr;
     HWND nextTrackButton_ = nullptr;
+    HWND rebuildGraphButton_ = nullptr;
+    HWND renderOfflineButton_ = nullptr;
 
     HWND automationCheckbox_ = nullptr;
     HWND pdcCheckbox_ = nullptr;
     HWND anticipativeCheckbox_ = nullptr;
 
     HWND statusLabel_ = nullptr;
-    HWND backendLabel_ = nullptr;
-    HWND sampleRateLabel_ = nullptr;
-    HWND blockSizeLabel_ = nullptr;
-    HWND cpuLabel_ = nullptr;
-    HWND xrunsLabel_ = nullptr;
-    HWND deadlineMissesLabel_ = nullptr;
-    HWND graphVersionLabel_ = nullptr;
-    HWND latencyLabel_ = nullptr;
-    HWND transportLabel_ = nullptr;
+    HWND tempoLabel_ = nullptr;
+    HWND patternLabel_ = nullptr;
+    HWND snapLabel_ = nullptr;
+    HWND systemLabel_ = nullptr;
+    HWND hintsLabel_ = nullptr;
     HWND projectSummaryLabel_ = nullptr;
     HWND documentLabel_ = nullptr;
     HWND selectionLabel_ = nullptr;
-    HWND trackListLabel_ = nullptr;
-    HWND busListLabel_ = nullptr;
-    HWND errorLabel_ = nullptr;
+
+    HWND browserMenuButton_ = nullptr;
+    HWND browserPanel_ = nullptr;
+    HWND channelRackMenuButton_ = nullptr;
+    HWND channelRackPanel_ = nullptr;
+    HWND stepSequencerPanel_ = nullptr;
+    HWND pianoRollMenuButton_ = nullptr;
+    HWND pianoRollPanel_ = nullptr;
+    HWND playlistMenuButton_ = nullptr;
+    HWND playlistPanel_ = nullptr;
+    HWND mixerMenuButton_ = nullptr;
+    HWND mixerPanel_ = nullptr;
+    HWND pluginMenuButton_ = nullptr;
+    HWND pluginPanel_ = nullptr;
 
     mutable VisibleEngineState visibleState_{};
+    WorkspaceState workspace_{};
     std::size_t selectedTrackIndex_ = 0;
 };
 
